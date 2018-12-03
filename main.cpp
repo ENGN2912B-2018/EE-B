2973ff2e099546a035933f77acccaa39c750553b
Alex Cannan <guest106@login003.oscar.ccv.brown.edu>
2018-12-03 17:49:38 -0500

    Generated CMake for kissfft, updated main.cpp. Segfault occuring

--- main.cpp
+++ main.cpp
@@ -20,12 +20,13 @@ int main(int argc, char *argv[])
   }
 
   cout << "Initializing vector for analysis" << endl;
-  unsigned vecSize = 5000;
-  kiss_fft_scalar input[vecSize];
+  unsigned vecSize = 1024; // ideally this should be a power of 2
+  cout << "about to create an array with kiss scalar type..." << endl; // This step in the main function creates a segmentation fault. Perhaps kiss_fft_scalar type is not initialized properly?
+  kiss_fft_scalar inputfft[vecSize];
   cout << "input vector of size " << vecSize << ": ";
 
   for (int i = 0; i < vecSize; i++) {
-    input[i] = data[i];
+    inputfft[i] = data[i];
   }
 		
   cout << "\nSuccessfully generated vector of length " << vecSize << endl;
@@ -35,14 +36,14 @@ int main(int argc, char *argv[])
   cfg = kiss_fftr_alloc(vecSize, 0,0,0);
   cout << "Pushed input vector into cx_in" << endl;
   cout << "Initializing output vector. Calling fftr" << endl;
-  kiss_fft_cpx output[vecSize];
+  kiss_fft_cpx outputfft[vecSize];
   cout << "HI" << endl;
-  kiss_fftr(cfg, input, output);
+  kiss_fftr(cfg, inputfft, outputfft);
   cout << "Made it past fftr" << endl;
 	
   for (int i = 0; i < vecSize; i++) {
-    cout << "Mag = " <<sqrt(pow(output[i].r,2) + pow(output[i].i,2)) << " : ";
-    cout << output[i].r << " + " << output[i].i << "i";
+    cout << "Mag = " <<sqrt(pow(outputfft[i].r,2) + pow(outputfft[i].i,2)) << " : ";
+    cout << outputfft[i].r << " + " << outputfft[i].i << "i";
     cout << endl;
   }
 
