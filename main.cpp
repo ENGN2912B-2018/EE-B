1e7a95b7e419417c309372a81f560a4615718f6f
Domenic Ferri <guest115@node408.oscar.ccv.brown.edu>
2018-12-03 03:18:31 -0500

    Added Cmake and combind program

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
+#define FIXED_POINT 16
+
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 #include "FileReader.h"
+#include "kiss_fft.h"
+#include "tools/kiss_fftr.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    FileReader reader;
-    vector<int> data = reader.read("example2.wav");
+  FileReader reader;
+  vector<int> data = reader.read("example2.wav");
 
-    for (vector<int>::iterator i = data.begin(); i != data.end(); ++i)
-    {
-        //cout << *i << endl;
-    }
+  for (vector<int>::iterator i = data.begin(); i != data.end(); ++i){
+    //cout << *i << endl;
+  }
+
+  cout << "Initializing vector for analysis" << endl;
+  unsigned vecSize = 5000;
+  kiss_fft_scalar input[vecSize];
+  cout << "input vector of size " << vecSize << ": ";
+
+  for (int i = 0; i < vecSize; i++) {
+    input[i] = data[i];
+  }
+		
+  cout << "\nSuccessfully generated vector of length " << vecSize << endl;
+  cout << "Prepping kiss_fft for FFT" << endl;
+
+  kiss_fftr_cfg cfg;
+  cfg = kiss_fftr_alloc(vecSize, 0,0,0);
+  cout << "Pushed input vector into cx_in" << endl;
+  cout << "Initializing output vector. Calling fftr" << endl;
+  kiss_fft_cpx output[vecSize];
+  cout << "HI" << endl;
+  kiss_fftr(cfg, input, output);
+  cout << "Made it past fftr" << endl;
+	
+  for (int i = 0; i < vecSize; i++) {
+    cout << "Mag = " <<sqrt(pow(output[i].r,2) + pow(output[i].i,2)) << " : ";
+    cout << output[i].r << " + " << output[i].i << "i";
+    cout << endl;
+  }
+
+  
 }
 
 //http://soundfile.sapp.org/doc/WaveFormat/
---
+++ src/FileReader.cpp
@@ -0,0 +1,103 @@
+#include "FileReader.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+FileReader::FileReader()
+{
+    //ctor
+}
+
+FileReader::~FileReader()
+{
+    //dtor
+}
+
+vector<int> FileReader::read(string fileName)
+{
+    char * ChunkID = new char[4];
+    char * ChunkSize = new char[4];
+    char * Format = new char[4];
+    char * SubChunk1ID = new char[4];
+    char * SubChunk1Size = new char[4];
+    char * AudioFormat = new char[2];
+    char * NumChannels = new char[2];
+    char * SampleRate = new char[4];
+    char * ByteRate = new char[4];
+    char * BlockAlign = new char[2];
+    char * BitsPerSample = new char[2];
+    char * SubChunk2ID = new char[4];
+    char * SubChunk2Size = new char[4];
+    char * sample1 = new char[4];
+
+    typedef unsigned char u8;  // in case char is signed by default on your platform
+    typedef char u9;
+
+    ifstream ifstr(fileName.c_str(), ifstream::binary);
+    ifstr.read(ChunkID,4);
+    cout << ChunkID << endl;
+
+    ifstr.read(ChunkSize,4);
+    unsigned num = ((u8)ChunkSize[3] << 24) | ((u8)ChunkSize[2] << 16) | ((u8)ChunkSize[1] << 8) | (u8)ChunkSize[0];
+    cout << num << endl;
+
+    ifstr.read(Format,4);
+    cout << Format << endl;
+
+    ifstr.read(SubChunk1ID,4);
+    cout << SubChunk1ID << endl;
+
+    ifstr.read(SubChunk1Size,4);
+    num = ((u8)SubChunk1Size[3] << 24) | ((u8)SubChunk1Size[2] << 16) | ((u8)SubChunk1Size[1] << 8) | (u8)SubChunk1Size[0];
+    cout << num << endl;
+
+    ifstr.read(AudioFormat,2);
+    num = 0;
+    num = (((u8)AudioFormat[1] << 8) | (u8)AudioFormat[0]);
+    cout << num << endl;
+
+    ifstr.read(NumChannels,2);
+    num = 0;
+    num = (((u8)NumChannels[1] << 8) | (u8)NumChannels[0]);
+    cout << num << endl;
+
+    ifstr.read(SampleRate,4);
+    num = (((u8)SampleRate[3] << 24) | ((u8)SampleRate[2] << 16) | ((u8)SampleRate[1] << 8) | (u8)SampleRate[0]);
+    cout << num << endl;
+
+    ifstr.read(ByteRate,4);
+    num = (((u8)ByteRate[3] << 24) | ((u8)ByteRate[2] << 16) | ((u8)ByteRate[1] << 8) | (u8)ByteRate[0]);
+    cout << num << endl;
+
+    ifstr.read(BlockAlign,2);
+    num = 0;
+    num = (((u8)BlockAlign[1] << 8) | (u8)BlockAlign[0]);
+    cout << num << endl;
+
+    ifstr.read(BitsPerSample,2);
+    num = 0;
+    num = (((u8)BitsPerSample[1] << 8) | (u8)BitsPerSample[0]);
+    cout << num << endl;
+
+    ifstr.read(SubChunk2ID,4);
+    cout << SubChunk2ID << endl;
+
+    ifstr.read(SubChunk2Size,4);
+    num = ((u8)SubChunk2Size[3] << 24) | ((u8)SubChunk2Size[2] << 16) | ((u8)SubChunk2Size[1] << 8) | (u8)SubChunk2Size[0];
+    cout << num << endl;
+    int msize = num;
+
+    vector<int> data;
+    char * sample = new char[4];
+    int sampleInt;
+    for(unsigned i = 0; i < msize; i+=2)
+    {
+        ifstr.read(sample,2);
+        sampleInt = 0;
+        sampleInt = (((u9)sample[1] << 8) | (u9)sample[0]);
+        data.push_back(sampleInt);
+    }
+    return data;
+}
