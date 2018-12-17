e2edfbff6f27430b601300c8efbac5c56c100a68
Domenic Ferri <guest115@node405.oscar.ccv.brown.edu>
2018-12-17 01:34:13 -0500

    Added comments (more to come)

--- main.cpp
+++ main.cpp
@@ -16,15 +16,18 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+  // The FileReader class reads in the header and data from a .wav file.
   FileReader reader;
   vector<int> data = reader.read("../wavfiles/example2.wav");
 
+  // The FFTAnalyzer takes the FFT of the .wav file for data. The fileAnalyze method outputs the data in 2D vector format.
   FFTAnalyzer FFTtest(1024, 5, reader.getsamplerate());
   vector<vector<int> > analysis = FFTtest.fileAnalyze(data);
 
+  // The FeedbackID class calculates the probability of feedback occuring at each frequancy for each sample.
   FeedbackID IDtest(analysis);
   vector<vector<int> > FBProbs = IDtest.findFeedback();
         
 
-} //http://soundfile.sapp.org/doc/WaveFormat/
+} // http://soundfile.sapp.org/doc/WaveFormat/
 
