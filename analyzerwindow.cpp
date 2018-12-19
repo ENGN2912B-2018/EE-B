af57d2006a1f38bf150b3ac074823c29a656b4c9
Alex Cannan <alex_cannan@brown.edu>
2018-12-19 16:45:48 -0500

    Installed some file outputs in FFTanalyzer

--- analyzerwindow.cpp
+++ analyzerwindow.cpp
@@ -77,10 +77,6 @@ ui->progressBar->setValue(20);
     FFTAnalyzer FFTtest(1024, guiAnalysisPeriod, reader.getsamplerate());
     vector<vector<int> > analysis = FFTtest.fileAnalyze(data);
 
-//For purposes of plotting only:
-    //FFTAnalyzer FFTdata(1024,1,reader.getsamplerate());
-    //vector<vector<int> > m2plot = FFTdata.fileAnalyze(data);
-
 ui->progressBar->setValue(40);
 
     FeedbackID IDtest(analysis);
@@ -97,19 +93,22 @@ ui->progressBar->setValue(70);
 
 //output analysis matricies to analyze
 std::ofstream m2plot;
+std::ofstream m2analyze;
 m2plot.open("fftmatrix.csv");
+m2analyze.open("signaldata.csv");
 for (int i = 0; i < analysis.size(); i++) {
     for (int j = 0; j < analysis[1].size()-1; j++){
         m2plot << analysis[i][j] << ", ";
+        m2analyze << FFTtest.signaldata[i][j] << ", ";
     }
     m2plot << analysis[i][1023] << std::endl;
+    m2analyze << FFTtest.signaldata[i][1023] << std::endl;
 }
 
 try {
     unsigned int iW = IDtest.iWidth;
     unsigned int iH = IDtest.iHeight;
     const unsigned long len = iW*iH;
-    std::cout << "len = " << len << std::endl;
     int mag = 0;
     int fbmag = 0;
     // encode matrix data into file
@@ -140,67 +139,6 @@ catch (GnuplotException ge) {
 
 ui->progressBar->setValue(100);
 }
-
-/*
-void AnalyzerWindow::PlotSpectrum(std::vector<std::vector<int> > a, std::vector<std::vector<int> > p)
-{
-
-    QScatterSeries *scatterL;
-    QScatterSeries *scatterM;
-    QScatterSeries *scatterH;
-    QScatterSeries *scatterFB;
-
-    scatterL = new QScatterSeries();
-    scatterM = new QScatterSeries();
-    scatterH = new QScatterSeries();
-    scatterFB = new QScatterSeries();
-
-    int Lthresh = 5;
-    int Mthresh = 200;
-    int FBthresh = 29490;
-
-    for (unsigned x = 0; x < a.size(); x++) {
-        for (unsigned y = 1; y < 512; y++) {
-            if (a[x][y] < Lthresh && p[x][y] < FBthresh) {
-                scatterL->append(x,y);
-            } else if (a[x][y] >= Lthresh && a[x][y] < Mthresh && p[x][y] < FBthresh) {
-                scatterM->append(x,y);
-            } else if (a[x][y] >= Mthresh && p[x][y] < FBthresh) {
-                scatterL->append(x,y);
-            } else {
-                scatterFB->append(x,y);
-            }
-
-
-        }
-    }
-
-    scatterL->setMarkerShape(QScatterSeries::MarkerShapeRectangle);
-    scatterM->setMarkerShape(QScatterSeries::MarkerShapeRectangle);
-    scatterH->setMarkerShape(QScatterSeries::MarkerShapeRectangle);
-    scatterFB->setMarkerShape(QScatterSeries::MarkerShapeCircle);
-
-    scatterL->setMarkerSize(10.0);
-    scatterM->setMarkerSize(30.0);
-    scatterH->setMarkerSize(50.0);
-    scatterH->setColor("#00ff00");
-    scatterFB->setMarkerSize(40.0);
-    scatterFB->setColor("#ff0000");
-
-
-    chart->addSeries(scatterL);
-    chart->addSeries(scatterM);
-    chart->addSeries(scatterH);
-    chart->addSeries(scatterFB);
-
-    chart->createDefaultAxes();
-
-    cv->show();
-
-
-
-}
-*/
 
 void AnalyzerWindow::on_pushBrowse_clicked()
 {
--- src/FFTAnalyzer.h
+++ src/FFTAnalyzer.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include <iomanip>
 #include <math.h>
+#include <kiss_fft130/kiss_fft.h>
+#include <kiss_fft130/_kiss_fft_guts.h>
+#include <kiss_fft130/tools/kiss_fftr.h>
 
 using namespace std;
 
@@ -19,7 +22,16 @@ FFTAnalyzer(unsigned vS, unsigned aP, unsigned sR): vecSize(vS), analysisPeriod(
 
 ~FFTAnalyzer() {}
 
-std::vector<std::vector<int> > fileAnalyze(std::vector<int> data){
+//vars
+    unsigned inputLength;
+    unsigned vecSize;
+    unsigned buffCount;
+    unsigned buffLeftover;
+    unsigned analysisPeriod;
+    unsigned sampleRate;
+    vector<vector<int> > signaldata;
+
+std::vector<std::vector<int> > fileAnalyze(std::vector<int> data) {
 	//std::cout << "Inside analyzer function." << std::endl;
 //define some vars
 	inputLength = data.size();
@@ -28,41 +40,55 @@ std::vector<std::vector<int> > fileAnalyze(std::vector<int> data){
 	bool isLeftovers;
 	vector<vector<int> > matrixoutput;
 	kiss_fft_scalar inputfft[vecSize];
+    vector<int> debuginput;
+    for (int i = 0; i < vecSize; i++) {debuginput.push_back(0);}
 	kiss_fft_cpx outputfft[vecSize];
 	kiss_fftr_cfg cfg;
-    cfg = kiss_fftr_alloc(vecSize,0,0,0);
-	//cout << "Vars defined/allocated" << endl;
-//analyze chunks periodically
-	if (buffLeftover > 0) {buffCount = buffCount + 1; isLeftovers = true;} // if leftovers exist, increase buffer count by 1
-	for (int i = 0; i < buffCount; i++) {
-		//cout << "Chunk = " << i+1 << "/" << buffCount;
-		if (i%analysisPeriod == 0) {
-			if ((i+1 == buffCount) && (isLeftovers == true)) { // pad with zeros if at last chunk
-				//cout << "inside leftover if statement" << endl;
-				for (int k = 0; k < vecSize; k++) { // fill with zeros
-					inputfft[k+1] = 0;
-				}
-				for (int k = 0; k < buffLeftover; k++) { // fill beginning with leftovers
-					inputfft[k+1] = data[(i*vecSize)+1];
-				}
-			} else {
-				//cout << "Inside normal if statement" << endl;
-				for (int k = 0; k < vecSize; k++) {
-					int l = (i*vecSize)+1+k;
-					//cout << "Filling " << k+1 << "/" << vecSize << " with data[" << l << "] = " << data[l] << endl;
-					inputfft[k] = data[l];
-				}
-			}
+    cfg = kiss_fftr_alloc(vecSize,0,NULL,NULL);
+////analyze chunks periodically
+    if (buffLeftover > 0) {buffCount = buffCount + 1; isLeftovers = true;} // if leftovers exist, increase buffer count by 1
+    for (int i = 0; i < buffCount; i++) {
+
+        if (i%analysisPeriod == 0) {
+
+            if ((i+1 == buffCount) && (isLeftovers == true)) { // pad with zeros if at last chunk
+                //if final chunk & leftovers exist, fill with zeros
+                for (int k = 0; k < vecSize; k++) { // fill with zeros
+                    inputfft[k+1] = 0;
+                    debuginput[k+1] = 0;
+                }
+                //push leftovers into vector from the front
+                for (int k = 0; k < buffLeftover; k++) { // fill beginning with leftovers
+                    inputfft[k+1] = data[(i*vecSize)+1];
+                    debuginput[k+1] = data[(i*vecSize)+1];
+                }
+
+            } else {
+                //fill input buffer normally
+                for (int k = 0; k < vecSize; k++) {
+                    unsigned long l = (i*vecSize)+1+k;
+                    //cout << "Filling " << k+1 << "/" << vecSize << " with data[" << l << "] = " << data[l] << endl;
+                    inputfft[k] = data[l];
+                    debuginput[k] = data[l];
+                }
+
+            }
+
+//    for (int k = 0; k < vecSize; k++) {
+//        double intcos = cos((PI/2)*k);
+//        inputfft[k] = (int)(intcos);
+//        debuginput.push_back((int)(intcos));
+//    }
 			//cout << "about to call fftr... " << endl;
 //TODO: Apply window to input fft here!
-//            for (int i = 0; i < vecSize; i++) {
-//                double multiplier = 0.5 * (1 - cos(2*PI*i/(vecSize-1)));
-//                inputfft[i] = multiplier * inputfft[i];
-//            }
+            for (int i = 0; i < vecSize; i++) {
+                double multiplier = 0.5 * (1 - cos(2*PI*i/(vecSize-1)));
+                inputfft[i] = multiplier * inputfft[i];
+            }
+            signaldata.push_back(debuginput);
+
 			kiss_fftr(cfg, inputfft, outputfft);
-			//cout << "initializing output vector to be pushed into matrix" << endl;
 			vector<int> temp;
-			//cout << "MADE IT HERE!!" << endl;
 			for (int k = 0; k < vecSize; k++) {
 				//cout << "find magnitude of " << outputfft[k+1].r << " + i" << outputfft[k+1].i << endl;
                 double absmag = sqrt(pow(outputfft[k+1].r,2) + pow(outputfft[k+1].i,2));
@@ -73,33 +99,17 @@ std::vector<std::vector<int> > fileAnalyze(std::vector<int> data){
                 dbmag = (dbmag+noisefloor*1.00001)*(32000/noisefloor); // translate [-nf,0] to [0,nf] and then [0,32787]
                 temp.push_back((int)(dbmag));
 			}
-            //Smooth out the frequencies by averaging
-//            for (int i = 0; i < temp.size(); i++) {
-//                if (i > 2 && i < vecSize-3) {
-//                    temp[i] = (temp[i-2]+temp[i-1]+temp[i]+temp[i+1]+temp[i+2])/5;
-//                }
-//            }
-			//cout << "Filled vector with magnitude of fftr. Pushing to output matrix... " << endl;
+
 			matrixoutput.push_back(temp);
-			//cout << "pushed back! i = " << i << endl;
-		}
-	}
+        }
+    }
 float time = (float)(inputLength)/(float)(sampleRate);
 float period = (float)(vecSize)*(float)(analysisPeriod)/(float)(sampleRate);
-cout << fixed << setprecision(3);	
-cout << "Analyzed " << time << "s of audio every " << period << "s" << endl;
-cout << "Returning output matrix of dimensions " << matrixoutput.size() << "x" << matrixoutput[0].size() << "." << endl;
+std::cout << fixed << setprecision(3);
+std::cout << "Analyzed " << time << "s of audio every " << period << "s" << endl;
+std::cout << "Returning output matrix of dimensions " << matrixoutput.size() << "x" << matrixoutput[0].size() << "." << endl;
 	return matrixoutput;
-
 }
-
-	//vars	
-		unsigned inputLength;
-		unsigned vecSize;
-		unsigned buffCount;
-		unsigned buffLeftover;
-		unsigned analysisPeriod;
-		unsigned sampleRate;
 
     protected:
 
